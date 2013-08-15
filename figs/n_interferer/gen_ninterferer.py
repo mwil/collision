Fixed off-by-one error in the code

figs/n_interferer/gen_ninterferer.py
@@ -59,10 +59,10 @@ def gen_n_interf(mode='n'):
 			phi_range = np.random.uniform(-np.pi, np.pi, size=nphi*ninterf).reshape(ninterf, nphi)
 		
 		if content in ('unif',):
-			send_syms = np.random.randint(15, size=(ninterf+1)*(nsyms+2)).reshape(ninterf+1, nsyms+2)
+			send_syms = np.random.randint(16, size=(ninterf+1)*(nsyms+2)).reshape(ninterf+1, nsyms+2)
 		else:
 			# everyone gets the same symbols to send
-			sync_syms = np.random.randint(15, size=nsyms+2)
+			sync_syms = np.random.randint(16, size=nsyms+2)
 			send_syms = np.vstack([sync_syms]*(ninterf+1))
 
 		send_syms_s = send_syms[0][1:-1]

figs/ser_contour/gen_ser_contour.py
@@ -38,10 +38,10 @@ for tau in tau_range:
           
     # symbols of (synch'ed, unsynch'ed) sender
     if content in ('same',):
-        tmp_syms = np.random.randint(15, size=nsyms+2)
+        tmp_syms = np.random.randint(16, size=nsyms+2)
         send_syms = [tmp_syms, tmp_syms]
     else:
-        send_syms = [np.random.randint(15, size=nsyms+2), np.random.randint(15, size=nsyms+2)]
+        send_syms = [np.random.randint(16, size=nsyms+2), np.random.randint(16, size=nsyms+2)]
 
     send_syms_s, send_syms_u = send_syms[0][1:-1], send_syms[1][1:-1]  
     send_chips = map_chips(*send_syms)